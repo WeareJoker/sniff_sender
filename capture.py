b025b19caf7d0f45b10dbbb380d27b352779b5b5
kcrong <kcrong@kim82536.pe.kr>
2016-09-08 22:16:04 +0900

    Add Celery at `capture.py` & `send_client.py`

--- capture.py
+++ capture.py
@@ -1,9 +1,11 @@
+import time
 import pcapy
+
+from send_client import send_file
 
 # list all the network devices
 
 pcap_path = './pcap/'
-
 
 pc = pcapy.open_live(
     "any",  # Just all packet
@@ -14,6 +16,9 @@ pc = pcapy.open_live(
 
 # make filename list
 filename_list = [pcap_path + "output%d.pcap" % i for i in range(1, 2)]
+
+# make celery job list
+job_dict = dict()
 
 for filename in filename_list:
     dumper = pc.dump_open(filename)
@@ -32,3 +37,17 @@ for filename in filename_list:
     # Capture count at limit
     print(filename + " Saved!")
     # Call celery task
+    job_dict[filename] = send_file.delay(filename)
+
+
+# Check Job
+job_finish = False
+
+while job_finish is not True:
+    for job_pcapname in job_dict:
+        if job_dict[job_pcapname].status != 'SUCCESS':
+            print("Undo!! %s : %s" % (job_pcapname, job_dict[job_pcapname].status))
+            time.sleep(10)
+            break
+    else:
+        job_finish = True
--- send_client.py
+++ send_client.py
@@ -1,10 +1,14 @@
 from socket import *
+from celery import Celery
 from functools import partial
+
+app = Celery('send_client', backend='amqp', broker='amqp://guest@localhost//')
 
 host = 'localhost'
 port = 9999
 
 
+@app.task
 def send_file(filename):
     s = socket(AF_INET, SOCK_STREAM)
     s.connect((host, port))
@@ -13,5 +17,7 @@ def send_file(filename):
             print(pcap_chunk)
             s.send(pcap_chunk)
 
+
 if __name__ == '__main__':
-    send_file('pcap/output1.pcap')
+    result = send_file.delay('pcap/output1.pcap')
+    print(result.status)
